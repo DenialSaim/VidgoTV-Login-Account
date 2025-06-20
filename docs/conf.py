fc96552ac401631a440bbd2b1ee8b9239e322298
DenialSaim <RowanChristian20241@outlook.com>
2025-06-20 18:50:06 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+project = 'Vidgo TV'
+author = 'Denial'
+release = '1.0'
+ 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+ 
+html_theme = 'alabaster'
+html_static_path = ['_static']   
+ 
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '_static/vidgo-tv.jpg'
+ 
+ 
+ 
+chatbot
+ 
+ 
+ 
+document.addEventListener("DOMContentLoaded", function () {
+    var ze = document.createElement('script');
+    ze.id = 'ze-snippet';
+    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
+    document.body.appendChild(ze);
+});
