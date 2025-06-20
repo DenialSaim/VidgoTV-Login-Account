227ef9fafbb28d1e9be73d8ced9c6c009be792d0
DenialSaim <RowanChristian20241@outlook.com>
2025-06-20 18:51:00 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,16 +13,4 @@ html_js_files = [
     'chatbot.js',
 ]
 html_favicon = '_static/vidgo-tv.jpg'
- 
- 
- 
-chatbot
- 
- 
- 
-document.addEventListener("DOMContentLoaded", function () {
-    var ze = document.createElement('script');
-    ze.id = 'ze-snippet';
-    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
-    document.body.appendChild(ze);
-});
+
